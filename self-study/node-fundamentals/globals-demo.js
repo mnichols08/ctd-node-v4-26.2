7c6e21fb4db42168be494c2a3e6f028161be72f6
Mikey Nichols <mnix@journeytocode.io>
2026-04-02 22:55:24 -0400

    add globals-demo.js to demonstrate Node.js global variables

---
+++ self-study/node-fundamentals/globals-demo.js
@@ -0,0 +1,11 @@
+console.log("__dirname:", __dirname);
+
+console.log("__filename:", __filename);
+
+console.log("Process ID:", process.pid);
+
+console.log("Platform:", process.platform);
+
+global.myCustomVar = "Hello, global!";
+
+console.log("Custom global variable:", global.myCustomVar);
